85689d9a782eabbb1bdcf276a08f96daab99f80c
hugthebug <playground177@gmail.com>
2020-01-21 16:08:09 +0300

    environmental variable, endpoint to debug in production

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const data = require('./data');
 const app = express();
 const port = process.env.PORT || 8080;
 
+const updatePosts = async () => {
+  logger.info('starting posts update');
+  data.getLastId()
+    .then(query.fetchUntilId)
+    .then(data.updateLastId)
+    .then(parser.filterLinks)
+    .then(data.addLinks)
+    .then(async (links) => logger.info(links));
+};
+
 app.get('/links', async (req, res) => res.status(200).json(await data.getLinks()));
 
 app.get('/lastid', async (req, res) => res.status(200).json(await data.getLastId()));
@@ -19,20 +29,18 @@ app.get('/', (req, res) => {
     .send('Hello, world!');
 });
 
+app.get('/forceupdate', (req, res) => {
+  updatePosts();
+  res
+    .status(200)
+    .send('forced update');
+});
+
 app.listen(port, () => {
   logger.info(`app listening on port ${port}`);
 
   const job = new cron.CronJob('00 00 08,20 * * *', () => {
-    logger.info('starting posts updating');
-    data.getLastId()
-      .then(query.fetchUntilId)
-      .then(data.updateLastId)
-      .then(parser.filterLinks)
-      .then(data.addLinks)
-      .then(async (links) => {
-        logger.info(links);
-        return links;
-      });
+    updatePosts();
   });
 
   job.start();
