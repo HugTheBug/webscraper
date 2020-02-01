576362d7f9a6eb892ad6ae1eb174f8a77b095baa
hugthebug <playground177@gmail.com>
2020-02-01 21:31:22 +0300

    changed telegram bot to work with cloud functions

--- telegrambot/main.py
+++ telegrambot/main.py
@@ -1,36 +1,13 @@
-from flask import Flask
 import telegram
-from dotenv import load_dotenv
 import os
-import json
-from threading import Thread
 
-load_dotenv()
-
-app = Flask(__name__)
-token = os.getenv('TELEGRAM_TOKEN')
+token = os.environ['TELEGRAM_TOKEN']
 bot = telegram.Bot(token=token)
 
-def process_webhook(content):
-    print('content {}'.foramt(content))
-    print('message {}'.format(content['message']))
-    message = content.message
-    print('from {}'.format(message['from']))
-    print('chat {}'.format(message['chat']))
-
-@app.route('/bot/')
-def bot_hello():
-    return 'bot here'
-
-@app.route('/bot/' + token)
-def webhook_arrived():
-    thread = Thread(target=process_webhook, args=(request.json,))
-    thread.start()
-    
-    return 'hi, webhook'
-
-if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8081, debug=True)
+def webhook(request):
+    if request.method == "POST":
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        chat_id = update.message.chat.id
+        # Reply with the same message
+        bot.sendMessage(chat_id=chat_id, text=update.message.text)
+    return "ok"
