5545748da7ff72a00a355654ce35c80c8df2df9a
hugthebug <playground177@gmail.com>
2020-02-02 17:43:02 +0300

    better command handling

--- telegrambot/main.py
+++ telegrambot/main.py
@@ -9,19 +9,29 @@ db = firestore.Client()
 token = os.environ['TELEGRAM_TOKEN']
 bot = telegram.Bot(token=token)
 
+def parse_message(message, chat_id):
+    text = message.text
+    parsed = False
+    for entity in message.entities:
+        if entity.type == 'bot_command':
+            command = message.text[entity.offset:entity.offset+entity.length]
+            print('received command {}'.format(command))
+            if command == '/id':
+                doc = db.collection(collectionScraping).document(documentTelegram).get()
+                bot.sendMessage(chat_id=chat_id, text=doc.to_dict()[u'lastId'])
+                parsed = True
+            elif command == '/echo':
+                bot.sendMessage(chat_id=chat_id, text=text)
+                parsed = True
+    return parsed
+
 def webhook(request):
     if request.method == 'POST':
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
         message = update.message
 
-        text = message.text
-        for entity in message.entities:
-            if entity.type == 'bot_command':
-                command = message.text[entity.offset:entity.offset+entity.length]
-                if command == 'id':
-                    doc = db.collection(collectionScraping).document(documentTelegram).get()
-                    bot.sendMessage(chat_id=chat_id, text=doc.to_dict()[u'lastId'])
-                elif command == 'echo':
-                    bot.sendMessage(chat_id=chat_id, text=text) 
+        if not parse_message(message, chat_id):
+            print('failed to parse the received message')
+            bot.sendMessage(chat_id=chat_id, text='???')   
     return 'ok'
